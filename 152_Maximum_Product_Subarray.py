a081519722e0dd8f0b84a3876f84a86be303092d
Xufei Gao <Gao@Xufeis-MacBook-Pro.local>
2019-09-08 03:38:38 -0400

    lc152

---
+++ 152_Maximum_Product_Subarray.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        l = len(nums)
+        left_product = 1
+        right_product = 1
+        max_product = float('-inf')
+        for i in range(l):
+            left_product *= nums[i]
+            right_product *= nums[l-j-1]
+            max_product = max(max_product,left_product)
+            max_product = max(max_product,right_product)
+            if left_product == 0:
+                left_product = 1
+            if right_product == 0:
+                right_product = 1
+        
+
+        return max_product
