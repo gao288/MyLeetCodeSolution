39f8e1fc6fb0184dc191885d5a732736bc1bef57
Felix Gao <home@Felixs-MacBook-Pro.local>
2019-10-05 21:03:57 -0400

    lc1192

--- 1192_Critical_Connections_in_a_Network.py
+++ 1192_Critical_Connections_in_a_Network.py
@@ -1,15 +1,8 @@
 #similar to Tarjan SSC(strongly connected components) but for finding bridges there is no need for a stack
+# https://www.youtube.com/watch?v=aZXi1unBdJA
+# https://www.youtube.com/watch?v=TyWtx7q2D7Y
 from typing import *
 from collections import defaultdict
-# class Node:
-#     def __init__(self,id):
-#         self.id = id
-#         self.link = []
-#         self.visit = 0
-#         self.lowlink = id
-#     def add_links(self,l):
-#         self.link.append(l)
-    
 
 class Solution:
     def __init__(self):
@@ -41,7 +34,7 @@ class Solution:
         return ret
             
         
-    def dfs(self,prenode,curr_node):
+    def dfs(self,pre_node,curr_node):
 
         if self.visited[curr_node] == 1:
             return self.lowlink[curr_node] 
@@ -50,7 +43,7 @@ class Solution:
         self.rank += 1
         self.visited[curr_node] = 1
         for e in self.dic[curr_node]:
-            if e != prenode:
+            if e != pre_node:
                 self.lowlink[curr_node] = min(self.lowlink[curr_node],self.dfs(curr_node,e))
 
         return self.lowlink[curr_node]
